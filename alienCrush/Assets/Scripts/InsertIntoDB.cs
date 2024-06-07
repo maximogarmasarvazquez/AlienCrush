7860a12150066287f36f7d9e7891f94b707d5ffa
MaxiVazquezg <maxivaz55@gmail.com>
2024-06-07 13:22:18 -0300

    listaUser update

--- alienCrush/Assets/Scripts/InsertIntoDB.cs
+++ alienCrush/Assets/Scripts/InsertIntoDB.cs
@@ -8,12 +8,11 @@ using UnityEngine.UI;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
 using UnityEngine.SocialPlatforms;
 using UnityEditor;
+using System;
 public class InsertIntoDB : MonoBehaviour
 {
     public GameObject UserTextPrefab; // Referencia al Prefab
     public Transform UserListContainer; // Referencia al contenedor
-    public InputField IDInput;
-    public Text NombreText;
     public string DatabaseNombre;
     public InputField NombreInput;
     public User user;
@@ -38,38 +37,6 @@ public class InsertIntoDB : MonoBehaviour
         while (reader.Read())
         {
 
-        }
-
-        reader.Close();
-        reader = null;
-        dbcmd.Dispose();
-        dbcmd = null;
-        dbcon.Close();
-        dbcon = null;
-    }
-
-
-    public void FindItem()
-    {
-
-        var _IDInput = IDInput.text.Trim();
-        NombreText.text = "";
-
-
-        string conn = SetDataBaseClass.SetDataBase(DatabaseNombre + ".db");
-        IDbConnection dbcon;
-        IDbCommand dbcmd;
-        IDataReader reader;
-
-        dbcon = new SqliteConnection(conn);
-        dbcon.Open();
-        dbcmd = dbcon.CreateCommand();
-        string SQLQuery = "Select Nombre FROM Users Where ID = '" + _IDInput + "'";
-        dbcmd.CommandText = SQLQuery;
-        reader = dbcmd.ExecuteReader();
-        while (reader.Read())
-        {
-            NombreText.text = reader.GetString(0);
         }
 
         reader.Close();
@@ -119,8 +86,47 @@ public class InsertIntoDB : MonoBehaviour
     }
 
     public void SelectUser(string nombre)
-    {
-        Debug.Log("Botón presionado: " + nombre);
-        // Aquí puedes añadir cualquier funcionalidad adicional que necesites al seleccionar un usuario
+    {   
+        user = new User();
+        string conn = SetDataBaseClass.SetDataBase(DatabaseNombre + ".db");
+        IDbConnection dbcon = new SqliteConnection(conn);
+        dbcon.Open();
+        IDbCommand dbcmd = dbcon.CreateCommand();
+
+        string SQLQuery = "SELECT * FROM Users WHERE Nombre = '" + nombre + "'";
+        dbcmd.CommandText = SQLQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+   
+        while (reader.Read())
+        {
+
+
+            user.Id = reader.GetInt32(0);
+            user.nombre = reader.GetString(1);
+
+            //ERROR
+            //int nivel1Value = reader.GetInt32(2);  // Asumimos que Nivel1 se almacena como entero 0 (falso) o 1 (verdadero) 
+            //user.nivel1 = nivel1Value != 0; //ERROR
+
+            //user.puntos = reader.GetInt32(3); //ERROR
+            //user.estrellas = reader.GetInt32(4); //ERROR
+
+            Debug.Log("ID: " + user.Id);
+            Debug.Log("Nombre: " + user.nombre);
+            //Debug.Log("Nivel 1: " + user.nivel1);
+            //Debug.Log("Puntos: " + user.puntos);
+            //Debug.Log("Estrellas: " + user.estrellas);
+
+
+        }
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbcon.Close();
+        dbcon = null;
     }
 }
+
+
